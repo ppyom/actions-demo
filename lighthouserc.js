d7ea3065bc5c0d5b7e33edb5d6fdf71cea3a777e
ppyom <ouob77@kakao.com>
2024-11-12 14:19:12 +0900

    [test] Lighthouse 설정파일 수정

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,14 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./build",
-      url: ["http://localhost:3001"],
-      numberOfRuns: 5,
+      url: ["http://localhost:3000"],
+      numberOfRuns: 2,
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 1 }],
+      },
     },
     upload: {
       target: "temporary-public-storage",
